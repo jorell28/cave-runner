c9407307d4b62481db1b9524ef15bfdc8cde9bb8
jorell28 <140922322+jorell28@users.noreply.github.com>
2024-01-13 12:56:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -41,22 +41,22 @@ let score = 0
 info.setLife(5)
 info.highScore()
 let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . f f f f . . . . . . . 
-    . . . f f e e e e f f . . . . . 
-    . . f e e e f f e e e f . . . . 
-    . . f f f f 2 2 f f f f . . . . 
-    . f f e 2 e 2 2 e 2 e f f . . . 
-    . f e 2 f 2 f f f 2 f e f . . . 
-    . f f f 2 f e e 2 2 f f f . . . 
-    . f e 2 f f e e 2 f e e f . . . 
-    f f e f f e e e f e e e f f . . 
-    f f e e e e e e e e e e f d f . 
-    . . f e e e e e e e e f f b f . 
-    . . e f f f f f f f f 4 f b f . 
-    . . 4 f 2 2 2 2 2 e d d f c f . 
-    . . e f f f f f f e e 4 f f . . 
-    . . . f f f . . f f f . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . f f e e e e f f . . . . 
+    . . . f e e e f f e e e f . . . 
+    . . f f f f f 2 2 f f f f f . . 
+    . . f f e 2 e 2 2 e 2 e f f . . 
+    . . f e 2 f 2 f f 2 f 2 e f . . 
+    . . f f f 2 2 e e 2 2 f f f . . 
+    . f f e f 2 f e e f 2 f e f f . 
+    . f e e f f e e e e f e e e f . 
+    . . f e e e e e e e e e e f . . 
+    . . . f e e e e e e e e f . . . 
+    . . e 4 f f f f f f f f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
 animation.runImageAnimation(
 mySprite,
@@ -172,19 +172,6 @@ game.onUpdateInterval(2000, function () {
     bobb.setPosition(Math.randomRange(0, 160), 0)
     bobb.setKind(SpriteKind.Diamond)
 })
-game.onUpdateInterval(800, function () {
-    bobb = sprites.createProjectileFromSide(img`
-        5 5 e 5 e e e 
-        e 5 e e e 5 5 
-        e e e e 5 e e 
-        5 5 e e e e 5 
-        e e e 5 e e e 
-        e 5 e e e 5 e 
-        e 5 e e 5 5 e 
-        `, 0, 70)
-    bobb.setPosition(Math.randomRange(0, 160), 0)
-    bobb.setKind(SpriteKind.Gold)
-})
 game.onUpdateInterval(400, function () {
     bobb = sprites.createProjectileFromSide(img`
         f f e f e e e 
@@ -197,6 +184,19 @@ game.onUpdateInterval(400, function () {
         `, 0, 70)
     bobb.setPosition(Math.randomRange(0, 160), 0)
     bobb.setKind(SpriteKind.Coal)
+})
+game.onUpdateInterval(900, function () {
+    bobb = sprites.createProjectileFromSide(img`
+        5 5 e 5 e e e 
+        e 5 e e e 5 5 
+        e e e e 5 e e 
+        5 5 e e e e 5 
+        e e e 5 e e e 
+        e 5 e e e 5 e 
+        e 5 e e 5 5 e 
+        `, 0, 70)
+    bobb.setPosition(Math.randomRange(0, 160), 0)
+    bobb.setKind(SpriteKind.Gold)
 })
 game.onUpdateInterval(500, function () {
     bobb = sprites.createProjectileFromSide(img`
